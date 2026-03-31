b4311f870196a639de351a7b15e2d08883e09a2a
MaggieForsythe <maggieforsythe1@gmail.com>
2026-03-31 15:31:50 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,39 +1,67 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+
 const app = express();
 
 app.use(express.json());
 app.use(express.static(__dirname));
 
+const DATA_FILE = path.join(__dirname, 'data.json');
+
+// Load data from file
+function loadData() {
+  try {
+    const data = fs.readFileSync(DATA_FILE);
+    return JSON.parse(data);
+  } catch {
+    return [];
+  }
+}
+
+// Save data to file
+function saveData(data) {
+  fs.writeFileSync(DATA_FILE, JSON.stringify(data, null, 2));
+}
+
+// homepage
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/test', (req, res) => {
-  res.send("API WORKING");
-});
-
-let appointments = [];
-
+// get appointments
 app.get('/appointments', (req, res) => {
+  const appointments = loadData();
   res.json(appointments);
 });
 
+// add appointment
 app.post('/appointments', (req, res) => {
   const { name, datetime } = req.body;
 
-const item = {
-  id: Date.now(),
-  name,
-  datetime
-};
+  const appointments = loadData();
+
+  const item = {
+    id: Date.now(),
+    name,
+    datetime
+  };
+
   appointments.push(item);
+  saveData(appointments);
+
   res.json(item);
 });
 
+// delete appointment
 app.delete('/appointments/:id', (req, res) => {
   const id = Number(req.params.id);
+
+  let appointments = loadData();
   appointments = appointments.filter(a => a.id !== id);
+
+  saveData(appointments);
+
   res.json({ ok: true });
 });
 
